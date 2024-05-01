0f2d3223be7a417d1fdbefbc7f92cb2606e1e53a
rurruur <kdkeiie8@gmail.com>
2024-05-01 22:32:33 +0900

    [web] feat: 게시글 페이지네이션

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -7,19 +7,27 @@ import {
   ListHeader,
   ListItem,
   ListIcon,
+  Pagination,
 } from "semantic-ui-react";
 import { useAuth } from "../auth";
 import { useNavigate } from "react-router-dom";
+import { useListParams } from "@sonamu-kit/react-sui";
+import { PostBaseListParams } from "src/services/sonamu.generated";
 
 export default function PublicIndexPage() {
   const navigate = useNavigate();
   const { user, loading } = useAuth();
 
-  const { data, isLoading: listLoading } = PostService.usePosts("P", {
+  const { register, listParams } = useListParams(PostBaseListParams, {
     num: 20,
     page: 1,
     orderBy: "id-desc",
   });
+
+  const { data, isLoading: listLoading } = PostService.usePosts(
+    "P",
+    listParams
+  );
 
   const { rows, total } = data ?? {};
 
@@ -37,28 +45,36 @@ export default function PublicIndexPage() {
       {listLoading || loading ? (
         <div>Loading...</div>
       ) : (
-        <List relaxed className="post-list">
-          {rows?.map((e) => (
-            <ListItem key={e.id} className="post-item">
-              <ListContent
-                as="a"
-                href={`/post/${e.id}`}
-                className="post-item-content"
-              >
-                <ListHeader className="post-item-header">
-                  <div>{e.title}</div>
-                  {e.file_url && <ListIcon name="image outline" />}
-                  <div className="post-item-comment-count">
-                    {e.comments.length}
-                  </div>
-                </ListHeader>
-                <ListDescription className="post-item-description">
-                  {e.name} | {e.created_at}
-                </ListDescription>
-              </ListContent>
-            </ListItem>
-          ))}
-        </List>
+        <>
+          <List relaxed className="post-list">
+            {rows?.map((e) => (
+              <ListItem key={e.id} className="post-item">
+                <ListContent
+                  as="a"
+                  href={`/post/${e.id}`}
+                  className="post-item-content"
+                >
+                  <ListHeader className="post-item-header">
+                    <div>{e.title}</div>
+                    {e.file_url && <ListIcon name="image outline" />}
+                    <div className="post-item-comment-count">
+                      {e.comments.length}
+                    </div>
+                  </ListHeader>
+                  <ListDescription className="post-item-description">
+                    {e.name} | {e.created_at}
+                  </ListDescription>
+                </ListContent>
+              </ListItem>
+            ))}
+          </List>
+          {total && listParams.num && (
+            <Pagination
+              totalPages={Math.ceil(total / listParams.num)}
+              {...register("page")}
+            />
+          )}
+        </>
       )}
     </>
   );
